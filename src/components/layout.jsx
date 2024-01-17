5727482488526825e39fd9970bf4f500bdebcb5c
danielgoldshtein <danigold054@gmail.com>
2024-01-17 12:49:29 +0200

    1

---
+++ src/components/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function layout() {
+  return (
+    <div>
+        
+      <Home/>
+      <Administrator/>
+      <User/>
+
+    </div>
+  )
+}
+
+export default layout
